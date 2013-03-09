abdf572356fbe30ff388ba3cb1dd890834f452ec
Michał Jakub Janiec <mjjaniec@Janiecs.(none)>
2013-03-09 10:42:18 +0100

    version 0.1

---
+++ try.c
@@ -0,0 +1,69 @@
+#include "try.h"
+
+__thread int  ____stat;
+__thread bool ____stack_trace;
+__thread char ____msg[____MSG_TAB_SIZE];
+
+#define size 4096            // a la constant :)
+void ___nth(int fd, int line){
+   int stat, cline=1, i, j;
+   bool nend=true, all=false;
+   char buff[size+1];                   //+ one for eventually null.
+   
+   while(nend){
+      stat=read(fd,buff,size);
+      if(stat==-1)return;
+      if(stat<size)nend=false,all=true;
+      for(i=0; cline<line && i<stat; ++i)
+         if(buff[i]=='\n')
+            ++cline;
+      if(cline==line)nend=false;      
+   }
+   
+   if(cline==line){
+      for(j=i+1; j<stat && buff[j]!='\n';++j);
+      if(j<stat)buff[j]='\0';   //stopped because of end line.
+      else{            
+         if(!all){               //not all line in buff.
+            buff[stat]='\0';
+            strcpy(buff,buff+i);
+            i=0;
+            read(fd,buff+i,size-i);
+            for(j=i+1; j<stat && buff[j]!='\n';++j);
+            if(j<stat)buff[j]='\0';     
+            else return;        //probably line is longer than 4096 or an error occurred.
+         }
+         else buff[stat]='\0';
+      }
+   }
+   fprintf(stderr,"%s",buff+i);
+}
+
+#undef size
+
+bool ___err(char *file, int line){
+  if(!errno)return false;
+  int fd, __errno;   
+  __errno=errno;
+  
+  if(!____stack_trace){
+    fprintf(stderr,"\n [%6d]  ERROR:\n",(int)getpid());
+    errno=__errno;
+    if(errno!=____NON_STANDARD_ERROR) perror("");
+    else fprintf(stderr,"%s",____msg);
+    fprintf(stderr,"\nerr at line: %s: %d: ",file, line);
+    fd=open(file,O_RDONLY);
+    if(fd!=-1)___nth(fd,line);
+    fprintf(stderr,"\n");
+    ____stack_trace=true;
+  }else{
+    fprintf(stderr,"called from: %s: %d: ",file, line);
+    fd=open(file,O_RDONLY);
+    if(fd!=-1)___nth(fd,line);
+    fprintf(stderr,"\n");
+  }
+  
+  errno=__errno;      
+  return true;
+}
+   
---
+++ try.h
@@ -0,0 +1,62 @@
+#ifndef ____TRY_H
+#define ____TRY_H
+
+/**
+ * Made by Michał Janiec
+ * 
+ * using
+ *      try systemcall()$;
+ *
+ * POSIX don't give uniform way to notify about errors so:
+ * $ is best way for most cases but there are no guarantee that it works properly.
+ * If function error code is different than 0 or -1 then it WONT be CAUGHT, by try and $.
+ * Moreover POSIX let functions set errno even if its executes successfully so proper value maybe misinterpreted.
+ * 
+ * Also you can declare RELEASE before including this file to erase all error checks.
+ * 
+ **/
+
+#include <sys/types.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#define null NULL
+
+#define ____err ___err(__FILE__,__LINE__)
+#define ____cond (____stat==-1 || ____stat==0) && errno!=EAGAIN && ____err
+#define err {if(____err)return false;}
+#define ____NON_STANDARD_ERROR 32000
+#define ____MSG_TAB_SIZE 1024
+
+#ifndef RELEASE
+   extern __thread int ____stat;
+   extern __thread bool ____stack_trace;
+   extern __thread char ____msg[];
+   #define ptry                 { ____stack_trace=false; ____stat=!(errno=(int)
+   #define try                  { ____stack_trace=false; errno=0; ____stat=(int)(
+   #define assert(cond,msg)		{ if(!(cond)){errno=____NON_STANDARD_ERROR; strncpy(___msg,msg,____MSG_TAB_SIZE)}
+   #define $                    );if(____cond)return false; }
+   #define $c                   );if(____cond);             }
+   #define $e                   );if(____cond)exit(1);      }
+   #define $$ 			printf("%s: %d\n",__FILE__, __LINE__);
+   
+#endif
+#ifdef RELEASE  
+   #define assert
+   #define ptry    
+   #define try
+   #define $
+   #define $c
+   #define $e
+   #define $$
+#endif
+
+void ___nth(int fd, int line);
+bool ___err(char *file, int line);
+
+#endif
